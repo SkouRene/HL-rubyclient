68fd632f91524abd8598a57f1a1f2eec12b2d580
skourene <skou.rene@gmail.com>
2016-10-23 11:44:44 +0200

    first commit

---
+++ lib/HL/rubyclient.rb
@@ -0,0 +1,7 @@
+require "HL/rubyclient/version"
+
+module HL
+  module Rubyclient
+    # Your code goes here...
+  end
+end
---
+++ lib/HL/rubyclient/version.rb
@@ -0,0 +1,5 @@
+module HL
+  module Rubyclient
+    VERSION = "0.1.0"
+  end
+end
